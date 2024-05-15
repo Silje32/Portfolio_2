65f695b8ec49a82c44c829baac89917af6393f6e
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-05-15 15:30:17 +0200

    Changed my color and background color

--- src/components/footer/Footer.styles.jsx
+++ src/components/footer/Footer.styles.jsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const StyledFooter = styled.footer`
-  color: black;
-  background-color: white;
+  color: white;
+  background-color: black;
   font-family: haboro-serif, serif;
   font-style: normal;
   font-weight: 500;
