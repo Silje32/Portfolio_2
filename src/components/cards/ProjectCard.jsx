cb6acfd885eafb2cb37b39a5201798cf12dbfe59
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-05-15 14:37:17 +0200

    Added description to my semester project 2

--- src/components/cards/ProjectCard.jsx
+++ src/components/cards/ProjectCard.jsx
@@ -15,6 +15,14 @@ function HomeCard() {
                 </StyledH2>
               </div>
               <div>
+                <p>An auction site is looking to launch a website where users can add items to be 
+                   bid on and bid on items other users have put up for auction. When a new user joins 
+                   the website, they are given 1000 credits to use on the site. They can get credits by 
+                   selling items and use credit by buying items. Non-registered users can search through the 
+                   listings, but only registered users can make bids on listings.
+                </p>
+              </div>
+              <div>
                 <Link to={"https://github.com/Silje32/Semester_Project_2.git"}>GITHUB REPOSITORY</Link>
               </div>
               <div>
