f0a45333e1d65590caf29b728a4eb39b8ad68939
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-05-15 15:19:05 +0200

    Added description to my Project Exam 2

--- src/components/cards/ExamCard.jsx
+++ src/components/cards/ExamCard.jsx
@@ -15,6 +15,14 @@ function ExamCard() {
                 </StyledH2>
               </div>
               <div>
+                <p>
+                   An existing Social Media company has approached you to create a brand new front 
+                   end for their application. While they have a list of required features, the design 
+                   and user experience has not been specified. Working with the official API 
+                   documentation, plan, design and build a modern front end social media application.
+                </p>
+              </div>
+              <div>
                 <Link to={"https://github.com/Silje32/Final_Project_Exam_2.git"}>GITHUB REPOSITORY</Link>
               </div> 
               <div>
