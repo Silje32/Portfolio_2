334955f4193631c920fa9b5bf42ae79b93a1606a
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-05-20 13:20:58 +0200

    Added media queries to my nav

--- src/components/layout/Nav.styles.jsx
+++ src/components/layout/Nav.styles.jsx
@@ -2,18 +2,17 @@ import styled from "styled-components";
 
 export const NavStyling = styled.nav`
   
-ul {
-   list-style-type: none;   
-   display: flex;
-   padding: 25px 10px;
-   min-width: 1470px;
-   cursor: pointer;
-   background-color: ${(props) => (props.$nav ? "white" : "black")};
+  ul {
+     list-style-type: none;   
+     display: flex;
+     padding: 25px 10px;
+     min-width: 1470px;
+     cursor: pointer;
+     background-color: ${(props) => (props.$nav ? "white" : "black")};
 
 }
 
-ul li {
-     
+  ul li {
       font-size: 18px;
       font-family: haboro-serif, times new roman, serif;
       font-style: normal;
@@ -22,12 +21,16 @@ ul li {
       margin: 0 auto;      
 }
 
-a {
-      color: ${(props) => (props.$nav ? "grey" : "white")}; 
+  a {
+   color: ${(props) => (props.$nav ? "grey" : "white")}; 
 }
    
-a:hover {
-      color: #4BA69D;
+  a:hover {
+       color: #4BA69D;
 }
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+      flex-direction: column;
+  }
 
 `;
