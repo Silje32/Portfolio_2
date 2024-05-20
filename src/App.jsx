e12aaa46b0c963ec417dcec7def5b32e340284ae
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-05-20 12:14:27 +0200

    Added ThemeProvider to my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import { Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import Home from "./pages/Home";
 import Layout from "./components/layout/Layout";
 import GlobalStyles from "./styles/GlobalStyles";
 
+const theme = {
+  colors: {
+    header: "#E8E0D1",
+    body: "#E8E0D1",
+    footer: "black",
+  },
+}
+
   
-  function App() {
-  
-    return (
-      <>
-        <GlobalStyles />
-        <Routes>
-          <Route path="/" element={<Layout />} >
-            <Route index element={<Home />} />
-            <Route path="home" element={<Home />} />
-          </Route>
-        </Routes>
-      </>
-    );
-  }
+function App() {
+  return (
+      <ThemeProvider theme = {theme}>
+        <>
+          <GlobalStyles />
+          <Routes>
+             <Route path="/" element={<Layout />} >
+             <Route index element={<Home />} />
+             <Route path="home" element={<Home />} />
+             </Route>
+          </Routes>
+        </>
+      </ThemeProvider>
+  );
+}
   
   export default App;  
