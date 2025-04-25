a93e1091cf9670de43cdb89e4c6ae0b1c6810649
Silje32 <msh070425@gmail.com>
2025-04-25 11:12:00 +0200

    Added the content

--- src/components/cards/FrameworksCard.jsx
+++ src/components/cards/FrameworksCard.jsx
@@ -12,20 +12,35 @@ function FrameworksCard() {
         </div>
         <div>
           <ul>
-            You are tasked with build out the following pages for an eCom store:
+            Build a frontend for the API and add the following:
             <li>Homepage</li>
-            <li>Individual product page</li>
-            <li>Cart page</li>
-            <li>Checkout success page</li>
-          </ul>
-          <div>
             <p>
-              The Homepage should have a list of all the products. There should
-              be a look-ahead search bar that filters products when typing in a
-              product name. Clicking on a product should take a user to an
-              individual product page.
+              Make a GET request to fetch a list of resources from your API.
             </p>
-          </div>
+            <p>
+              Create HTML for each item and display at least 3 properties for
+              each.
+            </p>
+            <p>
+              Each item should also display a button or icon. Clicking on this
+              button should toggle the item in and out of an array stored in
+              localStorage.
+            </p>
+            <p>
+              There should be a text input on this page that filters the array
+              of results on one of the properties.
+            </p>
+            <li>Favourites page</li>
+            <p>
+              This page should fetch the array of items stored in localStorage
+              and display them or display a message that there are no items.
+            </p>
+            <p>
+              There should be a "Clear all" button that clears localStorage (or
+              just a specific key in localStorage) and reloads the display.
+              Don´t reload the page, just redraw the HTML.
+            </p>
+          </ul>
         </div>
         <StyledLink>
           <Link to={"https://github.com/Silje32/Javascript-ca.git"}>
