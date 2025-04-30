43a58eb4826d512084ce67fe187b4166d7ce323a
Silje32 <msh070425@gmail.com>
2025-04-30 12:11:36 +0200

    Changed the link from netlify to github pages

--- src/components/buttons/FrameworksButton.jsx
+++ src/components/buttons/FrameworksButton.jsx
@@ -2,21 +2,17 @@ import { Link } from "react-router-dom";
 import { StyledFrameworksButton } from "./Buttons.styles";
 
 function FrameworksButton() {
+  function onButtonClick() {}
 
-    function onButtonClick() {
-        
-    }
-    
-
-    return ( 
-         <>
-           <StyledFrameworksButton $isActive={false} onClick={() => onButtonClick()}>
-               <Link to={"/frameworks"}>
-                   Go To Website
-               </Link>
-            </StyledFrameworksButton>
-         </>
-    );
+  return (
+    <>
+      <StyledFrameworksButton $isActive={false} onClick={() => onButtonClick()}>
+        <Link to={"https://silje32.github.io/Javascript-ca/"}>
+          Go To Website
+        </Link>
+      </StyledFrameworksButton>
+    </>
+  );
 }
 
 export default FrameworksButton;
